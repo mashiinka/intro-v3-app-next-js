e1c07b0b3f5c5a38574dc35ddf6bca2bfbba1a8b
mash-a <mariya.mego@finditparts.com>
2023-10-29 10:01:18 -0700

    Initial commit

---
+++ app/(dashboard)/todos/page.js
@@ -0,0 +1,6 @@
+const SettingsPage = ({params}) => {
+  console.log(params)
+  return <div>Todos Page</div>
+}
+
+export default SettingsPage;
---
+++ app/docs/[[...id]]/page.js
@@ -0,0 +1,8 @@
+import styles from './styles.module.css';
+
+const SlugsPage = ({ params }) => {
+  console.log(params)
+  return <div className={styles.red}>Docs</div>
+}
+
+export default SlugsPage;
